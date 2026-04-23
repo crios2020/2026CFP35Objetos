7ccb76fd633f8c23e180e45fca52aacd075545bf
Carlos <lawlercarlospatricio@gmail.com>
2026-04-23 19:46:36 -0300

    Clase12

---
+++ Clase12/entities/Figura.java
@@ -0,0 +1,7 @@
+package entities;
+
+public abstract class Figura{
+    public abstract double getPerimetro();
+    public abstract double getSuperficie();
+    public abstract String getEstado();
+}
---
+++ Clase12/test/TestDiagramaFigura2.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.text.DecimalFormat;
+
+import entities.Circulo;
+import entities.Rectangulo;
+import entities.Triangulo;
+
+public class TestDiagramaFigura2 {
+    public static void main(String[] args) {
+        
+        DecimalFormat df=new DecimalFormat("0.00");
+
+        System.out.println("-- Test Diagrama Figura 2 --");
+        
+        System.out.println("-- rectangulo2 --");
+        Rectangulo rectangulo1=new Rectangulo(30, 40);
+        System.out.println(rectangulo1.getEstado());
+        System.out.println("Perímetro: "+df.format(rectangulo1.getPerimetro()));
+        System.out.println("Superficie: "+df.format(rectangulo1.getSuperficie()));
+
+        System.out.println("-- triangulo1 --");
+        Triangulo triangulo1 = new Triangulo(30, 40);
+        System.out.println(triangulo1.getEstado());
+        System.out.println("Perímetro: "+df.format(triangulo1.getPerimetro()));
+        System.out.println("Superficie: "+df.format(triangulo1.getSuperficie()));
+
+        System.out.println("-- circulo1 --");
+        Circulo circulo1=new Circulo(50);
+        System.out.println(circulo1.getEstado());
+        System.out.println("Perímetro: "+df.format(circulo1.getPerimetro()));
+        System.out.println("Superficie: "+df.format(circulo1.getSuperficie()));
+    }
+}
